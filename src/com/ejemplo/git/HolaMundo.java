baa0d395fb38431c02209538413c22ad675f5f6c
abel huarca <abelhuarca@gmail.com>
2016-12-15 15:54:36 -0500

    Se actualizo la clase principal

--- src/com/ejemplo/git/HolaMundo.java
+++ src/com/ejemplo/git/HolaMundo.java
@@ -3,5 +3,6 @@ package com.ejemplo.git;
 public class HolaMundo {
 	  public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		System.out.println("Este es otro mensaje ");
 	}
 }
