a23187cb52dad3f2778380e2d19b400b7d8898a7
abel huarca <abelhuarca@gmail.com>
2016-12-15 16:00:00 -0500

    Cambios 15/12/2016

--- src/com/ejemplo/git/HolaMundo.java
+++ src/com/ejemplo/git/HolaMundo.java
@@ -5,5 +5,6 @@ public class HolaMundo {
 		System.out.println("Hola mundo");
 		System.out.println("Este es otro mensaje ");
 		System.out.println("Este es el mensaje numero 3");
+		System.out.println("Mensaje del eclipce normal");
 	}
 }
