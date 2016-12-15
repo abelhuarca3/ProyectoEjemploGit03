7ccc416e1fb5ec388d361054cba34bc0b807045f
abel huarca <abelhuarca@gmail.com>
2016-12-15 15:57:28 -0500

    Se crearon nuevas clases

--- src/com/ejemplo/git/HolaMundo.java
+++ src/com/ejemplo/git/HolaMundo.java
@@ -4,5 +4,6 @@ public class HolaMundo {
 	  public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Este es otro mensaje ");
+		System.out.println("Este es el mensaje numero 3");
 	}
 }
---
+++ src/com/ejemplo/git/Operaciones.java
@@ -0,0 +1,5 @@
+package com.ejemplo.git;
+
+public class Operaciones {
+
+}
